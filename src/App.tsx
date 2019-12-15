07bebf714887f65fb735eb557863f4c280ad3f40
Kelvin CS <kelvinh.job@gmail.com>
2019-12-15 12:47:37 -0300

    [components] Initial page created

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import GlobalStyles from "./styles/global";
+import Navbar from "./components/Navbar";
+import StockOptions from "./pages/StockOptions";
 
-const App: React.FC = () => {
-  return (
-    <main>
-      <GlobalStyles />
-    </main>
-  );
-};
+const App: React.FC = () => (
+  <main>
+    <GlobalStyles />
+    <Navbar />
+    <StockOptions />
+  </main>
+);
 
 export default App;
