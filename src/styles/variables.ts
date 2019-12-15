a36c32effe0554633d753a086e0620f5c5aa903f
Kelvin CS <kelvinh.job@gmail.com>
2019-12-14 23:47:53 -0300

    [style] Theme variables defined

---
+++ src/styles/variables.ts
@@ -0,0 +1,10 @@
+export const colors = {
+  primary: "#00ADD2",
+  dark: "#091827",
+  gray: "#ECEDEE",
+  grayDark: "#7E868E",
+  grayLight: "#D1D5D7",
+  high: "#00C853",
+  low: "#F44336",
+  white: "#ffffff"
+};
