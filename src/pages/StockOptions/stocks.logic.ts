5865d1fd40545e252b18f631bdf3f11c6bb8bcc4
Kelvin CS <kelvinh.job@gmail.com>
2019-12-15 12:25:11 -0300

    [logic] State management for stocks created

---
+++ src/pages/StockOptions/stocks.logic.ts
@@ -0,0 +1,96 @@
+import { kea } from "kea";
+import { put } from "redux-saga/effects";
+import StockService from "../../services/Stocks";
+
+interface Quote {
+  ticker: string;
+  price: number;
+  timestamp: number;
+}
+
+interface Stock {
+  ticker: string;
+  name: string;
+  image: string;
+  history?: { timestamp: number; price: number }[];
+}
+
+const stocks = kea({
+  actions: () => ({
+    addNewQuote: (quote: Quote) => quote,
+    updateStock: (stock: Stock) => stock,
+    retrieveStockInfo: (ticker: string) => ({ ticker })
+  }),
+
+  reducers: ({ actions }) => ({
+    stocksMap: [
+      {},
+      {
+        [actions.addNewQuote]: (state, quote: Quote) => {
+          const { ticker, price, timestamp } = quote;
+          const stockInfo = state[ticker] || { history: [], ticker };
+
+          return {
+            ...state,
+            [ticker]: {
+              ...stockInfo,
+              history: [
+                ...stockInfo.history.slice(-25),
+                { price: price, timestamp: timestamp }
+              ]
+            }
+          };
+        },
+        [actions.updateStock]: (state, stock: Stock) => {
+          const { ticker } = stock;
+          const { history } = state[ticker] || { history: [] };
+
+          return {
+            ...state,
+            [ticker]: {
+              ...stock,
+              history
+            }
+          };
+        }
+      }
+    ]
+  }),
+
+  selectors: ({ selectors }) => ({
+    stocks: [
+      () => [selectors.stocksMap],
+      stocksMap => Object.values(stocksMap),
+      []
+    ]
+  }),
+
+  takeEvery: ({ actions, workers }) => ({
+    NEW_QUOTE: workers.newQuote,
+    [actions.retrieveStockInfo]: workers.retrieveStockInfo
+  }),
+
+  workers: {
+    *newQuote(action) {
+      const { addNewQuote, retrieveStockInfo } = this.actions;
+
+      const { timestamp, ...tickers } = action.payload;
+      const [[ticker, price]] = Object.entries(tickers);
+
+      yield put(addNewQuote({ ticker, price, timestamp: Number(timestamp) }));
+
+      if (!this.values.stocksMap[ticker].name) {
+        yield put(retrieveStockInfo(ticker));
+      }
+    },
+
+    *retrieveStockInfo(action) {
+      const { updateStock } = this.actions;
+      const { ticker } = action.payload;
+
+      yield put(updateStock(StockService.getInfo(ticker)));
+    }
+  }
+});
+
+export default stocks;
