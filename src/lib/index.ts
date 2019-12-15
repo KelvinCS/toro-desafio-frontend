a8fca31232490f6a93f134d1e405d80fd42b8978
Kelvin CS <kelvinh.job@gmail.com>
2019-12-15 12:25:33 -0300

    [lib] Helper functions for stocks created

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,3 +3,24 @@ export const formatCurrency = (value: number): string => {
 };
 
 export const removeLastChar = (value: string) => value.slice(0, -1);
+
+const calcStockPriceIncrease = (stock: any) => {
+  const { price: openingPrice } = stock.history[0];
+  const { price: lastPrice } = stock.history.slice(-1)[0];
+
+  return lastPrice - openingPrice;
+};
+
+export const sortStockByPriceIncrease = (stocks: any[]) => {
+  return stocks.sort(
+    (stockA, stockB) =>
+      calcStockPriceIncrease(stockB) - calcStockPriceIncrease(stockA)
+  );
+};
+
+export const sortStockByPriceDecrease = (stocks: any[]) => {
+  return stocks.sort(
+    (stockA, stockB) =>
+      calcStockPriceIncrease(stockA) - calcStockPriceIncrease(stockB)
+  );
+};
