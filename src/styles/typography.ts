6e57909ea92f9120aeb30982ebd1bec4519e0e38
Kelvin CS <kelvinh.job@gmail.com>
2019-12-14 23:48:09 -0300

    [style] Typhography created

---
+++ src/styles/typography.ts
@@ -0,0 +1,28 @@
+import { colors } from "./variables";
+
+export const Title = `
+  font-size: 1.5rem;
+  color: ${colors.dark};
+  font-weight: 700;
+
+  @media (min-width: 768px) {
+    font-size: 2.625rem;
+  }
+`;
+
+export const Title2 = `
+  font-size: 1.5rem;
+  font-weight: 700;
+  color: ${colors.dark};
+`;
+
+export const Heading = `
+  font-size: 0.875rem;
+  color: ${colors.grayDark};
+  font-weight: 400;
+`;
+
+export const Text = `
+  font-size: 0.75rem;
+  font-weight: 600;
+`;
