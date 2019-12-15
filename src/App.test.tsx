af0ecf4ef1d08a529057b6345e3fc206e2b0f1f1
Kelvin CS <kelvinh.job@gmail.com>
2019-12-15 17:45:25 -0300

    [test] Test removed

--- src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { render } from "./lib/test-utils";
-
-test("renders learn react link", () => {
-  // const { getByText } = render(<h1>Opa</h1>);
-
-  expect("Opa").toBe("Opa");
-});
